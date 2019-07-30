d56bc725df6600936d51a73886a465e92c534a7b
苏观洲 <guanzhou.su@inin88.com>
2019-07-31 00:25:00 +0800

    首次提交web

---
+++ src/main/java/com/su/NormalWebApplication.java
@@ -0,0 +1,13 @@
+package com.su;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormalWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NormalWebApplication.class, args);
+	}
+
+}
