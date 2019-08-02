08067e5d79c9a7f2ae15e04b3700daebd5f8f2f8
苏观洲 <guanzhou.su@inin88.com>
2019-08-02 21:48:01 +0800

    Feat: 解决 springboot 应用打 war 包部署后无法启动问题，主要是增加一些依赖以及启动类增加处理

--- src/main/java/com/su/NormalWebApplication.java
+++ src/main/java/com/su/NormalWebApplication.java
@@ -1,13 +1,21 @@
 package com.su;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class NormalWebApplication {
+public class NormalWebApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NormalWebApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NormalWebApplication.class);
 	}
 
 }
